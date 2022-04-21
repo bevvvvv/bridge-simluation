649fc29b7eead6cb3dd7f3dee483c0b54caf10ce
Joseph Sepich <computerwhiz1337@gmail.com>
2022-04-20 21:20:15 -0400

    set contracting team

--- bridge.py
+++ bridge.py
@@ -14,38 +14,49 @@ def run_game() -> None:
     """ Run bridge game.
     """
     players = setup_game()
-    # TODO make actual game object to track total state
-    # Bid Sequence
-    current_contract, bid_winner = bid_sequence(players)
-    print("The winning bid is {} from player {}".format(str(current_contract), bid_winner))
-    
-    # setup scoreboard 
-    testTeams = [Team("Contracting", ["North", "South"], True), Team("Defending", ["West", "East"], False)]
-    
-    scoreboard = ScoreBoard(current_contract, testTeams)
-    
-    # play hands until cards run out
-    num_cards = 13
-    trick_results = []
-    while num_cards > 0:
-        current_trick = play_trick(players, "North", num_cards)
-        trick_results.append(current_trick)
-        winning_card, winning_player = current_trick.get_winner(current_contract.get_suit())
-        num_cards -= 1
-        print(winning_player, "wins the trick with", winning_card)
-    
-        if winning_player is "North" or winning_player is "South":
-            scoreboard.win_trick(testTeams[0], Trick(current_contract.get_suit()))
-        else:
-            scoreboard.win_trick(testTeams[1], Trick(current_contract.get_suit()))
-    
-    ns_wins, ew_wins = calculate_result(trick_results, current_contract.get_suit())
-    print("North/South won {} tricks meeting the contract suit {} times.".format(ns_wins[0], ns_wins[1]))
-    print("East/West won {} tricks meeting the contract suit {} times.".format(ew_wins[0], ew_wins[1]))
+    ns_vuln = False
+    eq_vuln = False
 
-    scoreboard.score_at_end_of_round()
-    
-    print(scoreboard)
+    while True:
+
+        # TODO make actual game object to track total state
+        # Bid Sequence
+        current_contract, bid_winner = bid_sequence(players)
+        print("The winning bid is {} from player {}".format(str(current_contract), bid_winner))
+        
+        # setup scoreboard 
+        ns_team_name = "Defending"
+        ew_team_name = "Contracting"
+        if bid_winner is "North" or bid_winner is "South":
+            ns_team_name = "Defending"
+            ew_team_name = "Contracting"
+        testTeams = [Team(ns_team_name, ["North", "South"], True), Team(ew_team_name, ["West", "East"], False)]
+        
+        scoreboard = ScoreBoard(current_contract, testTeams)
+        
+        # play hands until cards run out
+        num_cards = 13
+        trick_results = []
+        while num_cards > 0:
+            current_trick = play_trick(players, "North", num_cards)
+            trick_results.append(current_trick)
+            winning_card, winning_player = current_trick.get_winner(current_contract.get_suit())
+            num_cards -= 1
+            print(winning_player, "wins the trick with", winning_card)
+        
+            if winning_player is "North" or winning_player is "South":
+                scoreboard.win_trick(testTeams[0], Trick(current_contract.get_suit()))
+            else:
+                scoreboard.win_trick(testTeams[1], Trick(current_contract.get_suit()))
+        
+        ns_wins, ew_wins = calculate_result(trick_results, current_contract.get_suit())
+        print("North/South won {} tricks meeting the contract suit {} times.".format(ns_wins[0], ns_wins[1]))
+        print("East/West won {} tricks meeting the contract suit {} times.".format(ew_wins[0], ew_wins[1]))
+
+        scoreboard.score_at_end_of_round()
+        
+        print(scoreboard)
+        break
 
 def setup_game() -> Dict[str, Hand]:
     """ Setup a bridge game by dealing cards.
